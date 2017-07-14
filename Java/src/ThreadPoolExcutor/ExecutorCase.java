2faafdd0770990ce31717f032a17488846408d64
YinXinLION <yinxin19950816@gmail.com>
2017-07-14 21:40:08 +0800

    ~

---
+++ Java/src/ThreadPoolExcutor/ExecutorCase.java
@@ -0,0 +1,24 @@
+package ThreadPoolExcutor;
+
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+
+/**
+ * Created by yinxin on 17-7-14.
+ */
+public class ExecutorCase {
+
+    private static Executor executor = Executors.newFixedThreadPool(10);
+    public static void main(String[] args){
+        for (int i = 0; i < 20;i++){
+            executor.execute(new Task());
+        }
+    }
+
+    static class Task implements Runnable{
+        @Override
+        public void run(){
+            System.out.println(Thread.currentThread().getName());
+        }
+    }
+}
