8e92c12f7f41ea6da1e244c9d71bb5580ba21e6f
YinXinLION <yinxin19950816@gmail.com>
2017-07-21 17:41:24 +0800

    重载测试,发现参数相同返回值不同不可以

---
+++ Java/src/Base/Test1.java
@@ -0,0 +1,15 @@
+package Base;
+
+/**
+ * Created by yinxin on 17-7-21.
+ */
+public class Test1 {
+    public int hello(int a,int b){
+        return a+b;
+    }
+    /*
+    public double hello(int a,int b){
+        return a+b;
+    }
+    */
+}
