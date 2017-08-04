fa67d57ebffef99d9867e7166632fdf86fcd673d
YinXinLION <yinxin19950816@gmail.com>
2017-08-04 18:03:43 +0800

    字符串与数字的测试，字符串后的把数字也当字符串

---
+++ Java/src/Test/test1.java
@@ -0,0 +1,12 @@
+package Test;
+
+/**
+ * Created by yinxin on 17-7-26.
+ */
+public class test1 {
+    public static void main(String[] args) {
+        System.out.println("is "+ 100 + 5);
+        System.out.println(100 + 5 +" is" );
+        System.out.println("is "+ (100 + 5));
+    }
+}
